ad29c32210025ad2ffea162911cfcb64402ac656
Nurikhsan <ikhsanhikari29@gmail.com>
2019-06-25 17:06:48 +0700

    add controller

--- webserver/controller/product.go
+++ webserver/controller/product.go
@@ -27,47 +27,11 @@ func (h *handler) handleGetAllProducts(w http.ResponseWriter, r *http.Request, p
 	}
 
 	res := make([]view.DataResponse, 0, len(products))
-	for _, product := range products {
-		als, err := h.productLists.SelectByproduct(product.ID, pid)
-		if err != nil {
-			log.Println(err)
-			view.RenderJSONError(w, "Failed get lists", http.StatusInternalServerError)
-			return
-		}
-
-		ids := make([]int64, 0, len(als))
-		for _, al := range als {
-			ids = append(ids, al.ListID)
-		}
-
-		lists, err := h.lists.SelectByIDs(ids, pid)
-		if err != nil {
-			log.Println(err)
-			view.RenderJSONError(w, "Failed get lists", http.StatusInternalServerError)
-			return
-		}
-
-		names := make([]string, 0, len(lists))
-		for _, list := range lists {
-			names = append(names, list.Name)
-		}
-
-		tag := make([]string, 0, 10)
-		if product.Tags.Valid && product.Tags.String != "" {
-			tag = strings.Split(product.Tags.String, ",")
-		}
-
-		metaKeyword := make([]string, 0, 10)
-		if product.MetaKeywords.Valid && product.MetaKeywords.String != "" {
-			metaKeyword = strings.Split(product.MetaKeywords.String, ",")
-		}
-
-		
+	for _, product := range products {		
 		res = append(res, view.DataResponse{
 			Type: "products",
 			ID:   product.Product_id,
 			Attributes: view.ProductAttributes{
-				ProductId	: product.ProductId,
 				ProductName	: product.ProductName,
 				Description	: product.Description,
 				VenueTypeId	: product.VenueTypeId,
@@ -79,7 +43,6 @@ func (h *handler) handleGetAllProducts(w http.ResponseWriter, r *http.Request, p
 				Status      : product.Status,			
 				CreatedAt  	: product.CreatedAt,
 				UpdatedAt	: product.UpdatedAt,
-				ProjectId   : product.ProjectId
 			},
 		})
 	}
@@ -119,5 +82,97 @@ func (h *handler) handleDeleteProduct(w http.ResponseWriter, r *http.Request, ps
 		return
 	}
 
+	view.RenderJSONData(w, "OK", http.StatusOK)
+}
+
+func (h *handler) handlePostProduct(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var (
+		project, _ = authpassport.GetProject(r)
+		pid        = project.ID
+		params     reqArticle
+	)
+
+	err := form.Bind(&params, r)
+	if err != nil {
+		log.Println(err)
+		view.RenderJSONError(w, "Invalid parameter", http.StatusBadRequest)
+		return
+	}
+
+	product := product.Product{
+		ProductName	: params.ProductName,
+		Description	: params.Description,
+		VenueTypeId	: params.VenueTypeId,
+		Price		: params.Price,
+		Uom	        : params.Uom,
+		Currency	: params.Currency,
+		DisplayOrder: params.DisplayOrder,
+		Icon        : params.Icon,
+		Status      : params.Status,			
+		CreatedAt  	: params.CreatedAt,
+		UpdatedAt	: params.UpdatedAt,
+	}
+
+	err = h.product.Insert(&product)
+	if err != nil {
+		log.Println(err)
+		view.RenderJSONError(w, "Failed post product", http.StatusInternalServerError)
+		return
+	}
+	view.RenderJSONData(w, "OK", http.StatusOK)
+}
+
+func (h *handler) handlePatchProduct(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var (
+		project, _ = authpassport.GetProject(r)
+		pid        = project.ID
+		_id        = ps.ByName("id")
+		id, err    = strconv.ParseInt(_id, 10, 64)
+	)
+	if err != nil {
+		log.Println(err)
+		view.RenderJSONError(w, "Invalid parameter", http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.product.Get(id, pid)
+	if err == sql.ErrNoRows {
+		log.Println(err)
+		view.RenderJSONError(w, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil && err != sql.ErrNoRows {
+		log.Println(err)
+		view.RenderJSONError(w, "Failed get article", http.StatusInternalServerError)
+		return
+	}
+
+	err = form.Bind(&params, r)
+	if err != nil {
+		log.Println(err)
+		view.RenderJSONError(w, "Invalid parameter", http.StatusBadRequest)
+		return
+	}
+
+	product := product.Product{
+		ProductName	: params.ProductName,
+		Description	: params.Description,
+		VenueTypeId	: params.VenueTypeId,
+		Price		: params.Price,
+		Uom	        : params.Uom,
+		Currency	: params.Currency,
+		DisplayOrder: params.DisplayOrder,
+		Icon        : params.Icon,
+		Status      : params.Status,			
+		CreatedAt  	: params.CreatedAt,
+		UpdatedAt	: params.UpdatedAt,
+	}
+
+	err = h.product.Update(&product)
+	if err != nil {
+		log.Println(err)
+		view.RenderJSONError(w, "Failed update product", http.StatusInternalServerError)
+		return
+	}
 	view.RenderJSONData(w, "OK", http.StatusOK)
 }
