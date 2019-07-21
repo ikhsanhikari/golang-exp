0b817c092d0985f29d6b1f175e633cb27b978dc3
System Administrator <root@MacBook-Pro-2.local>
2019-07-22 01:21:31 +0700

    edit cert pdf

--- delivery/rest/controller/init.go
+++ delivery/rest/controller/init.go
@@ -180,4 +180,6 @@ func (c *Controller) Register(router *router.Router) {
 	router.PATCH("/companies/:id", c.auth.MustAuthorize(c.handlePatchCompany, "molanobar:companies.update"))
 	router.DELETE("/companies/:id", c.auth.MustAuthorize(c.handleDeleteCompany, "molanobar:companies.delete"))
 
+	//router.GET("/pdf", c.handleGetPdf1) //,"molanobar:email.ecert"))
+
 }
